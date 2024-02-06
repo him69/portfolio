1b4aa43dcbad073f1535b88c27ddd32e27ea7df8
him69 <crazybee376@gmail.com>
2024-02-06 22:34:36 +0530

    bacckend connected

--- pages/api/all.js
+++ pages/api/all.js
@@ -2,20 +2,18 @@ import { User } from "@/model/User";
 import { connectDb } from "@/utils/db";
 
 export default async function allProjectsByEmail(req, res) {
-  const userEmail = "gutamh142@gmail.com"; // Replace with the user's email
+  const userEmail = "gutamh142@gmail.com";
 
   try {
     await connectDb();
 
-    // Find the user by their email
     const user = await User.findOne({ email: userEmail });
 
     if (!user) {
-      // Handle the case where the user is not found
+
       return res.status(404).json({ success: false, message: "User not found" });
     }
 
-    // Access the "project" array within the user document
     const userProjects = user.project;
 
     res.status(200).json({
