0e2e866879ec777c6def5fbbb285bd6a2e763691
him69 <crazybee376@gmail.com>
2024-07-16 21:37:42 +0530

    updated the website

--- app/page.js
+++ app/page.js
@@ -62,10 +62,10 @@ const page = () => {
             <p className='pTextColor ls-lg text-lg font-semibold '>Web Developer & Designer</p>
             <h1 className='md:text-7xl text-5xl my-4 font-bold'>Himanshu <br></br> Gautam</h1>
             <p className='my-4'>As a skilled full-stack developer,I seamlessly transition between technologies ,leveraging AI tools like ChaptGPT to boost my coding speed effectivey. This portfolio, creafted with the aid of ChaptGPT, showcases my ability to seamlessly integrate AI with expertise in technology transitions. with a keen eye for detaild and a commitment to better thing's by <span className='pTextColor font-bold'>1%</span> </p>
-            <a href='' download className='border-2 border-white px-12 rounded text-2xl' style={{
+            {/* <a href='' download className='border-2 border-white px-12 rounded text-2xl' style={{
               paddingTop: '10px',
               paddingBottom: '10px'
-            }}>Resume</a>
+            }}>Feel more</a> */}
           </div>
         </div>
       </header>
@@ -96,20 +96,20 @@ const page = () => {
         <div className="myFeature md:flex block md:justify-between container mx-auto md:absolute relative md:left-1/2 md:right-1/2 md:-translate-x-1/2 translate-y-24" style={{ top: '18%' }}>
           <div className="md:w-1/3  pBgColor p-12 p-6 mx-4 my-8 md:my-0">
             <div className="flex justify-between">
-              <div className='icon pTextColor mr-4'>icon</div>
-              <div> <p className='text-xl mb-4 uppercase'>Sharp Observations</p> <p>I offer insights to enhance website and app usability. By understanding user needs, I provide suggestions for smoother navigation and better experiences.</p></div>
+              {/* <div className='icon pTextColor mr-4'>icon</div> */}
+              <div> <p className='text-xl mb-4 uppercase underline underline-offset-8'>Sharp Observations</p> <p>I offer insights to enhance website and app usability. By understanding user needs, I provide suggestions for smoother navigation and better experiences.</p></div>
             </div>
           </div>
           <div className="md:w-1/3  pBgColor p-12 p-6 mx-4 my-8 md:my-0">
             <div className="flex justify-between">
-              <div className='icon pTextColor mr-4'>icon</div>
-              <div> <p className='text-xl mb-4 uppercase'>Attention to Details</p> <p>I believe that the little things matter. So ,i pay close attention to every detail, like colors , fonts,and spacing. By getting these small things right,i ensure that the overall design looks polished and professional.</p></div>
+              {/* <div className='icon pTextColor mr-4'>icon</div> */}
+              <div> <p className='text-xl mb-4 uppercase underline underline-offset-8'>Attention to Details</p> <p>I believe that the little things matter. So ,i pay close attention to every detail, like colors , fonts,and spacing. By getting these small things right,i ensure that the overall design looks polished and professional.</p></div>
             </div>
           </div>
           <div className="md:w-1/3  pBgColor p-12 p-6 mx-4 my-8 md:my-0">
             <div className="flex justify-between">
-              <div className='icon pTextColor mr-4'>icon</div>
-              <div> <p className='text-xl mb-4 uppercase'>Continuous Improvement </p> <p>I'm always looking fofr ways to make my code and designs better, even if it's just by 1% whether it's tweaking code for efficiency or refining design elements for clarity, i'm committed to constantly imprving my works. This helps me stay on top of the latest trends and deliver the best possible results.</p></div>
+              {/* <div className='icon pTextColor mr-4'>icon</div> */}
+              <div> <p className='text-xl mb-4 uppercase underline underline-offset-8'>Continuous Improvement </p> <p>I'm always looking fofr ways to make my code and designs better, even if it's just by 1% whether it's tweaking code for efficiency or refining design elements for clarity, i'm committed to constantly imprving my works. This helps me stay on top of the latest trends and deliver the best possible results.</p></div>
             </div>
           </div>
         </div>
@@ -134,6 +134,37 @@ const page = () => {
             {/* midline */}
             <div className='midLine tBgColor'>
               <div className='topBall'></div>
+            </div>
+            {/* sapmle project */}
+            <div className="flex justify-center md:flex-row flex-col mx-3" >
+              <div className='md:w-1/2 relative'>
+                {/* Assuming 'projType' and other project-specific details are used here */}
+                <h4 className='md:text-right text-left text-2xl bLine tracking-widest px-2'>Kurv</h4>
+                <p className='md:text-right text-left pTextColor text-xl px-2'>Clothing</p>
+                <div className='projImg mt-4 md:ml-28 ml-24'>
+                  {/* Use project-specific image if available */}
+                  <img src="/images/kurv.png" alt="" />
+                </div>
+              </div>
+              <div className='pt-12 md:w-1/2 px-2'>
+                <p className='border-b w-fit mx-1 md:mx-0'>About Project</p>
+                <p className='pr-8 mt-4'> I worked on a Shopify project for the website <a href="https://kurv.in/" className='underline'>kurv.in</a>. My tasks included creating the website, integrating delivery partners, and setting up Razorpay as the payment option.</p>
+              </div>
+            </div>
+            {/* odd project */}
+            <div className="flex justify-center md:flex-row flex-col mx-3" >
+              <div className="pt-12 md:w-1/2 px-2 order-2 md:order-1">
+                <p className="abLine md:text-right text-left relative mx-1 md:mx-0">About Project</p>
+                <p className="mt-4 md:text-right text-left md:pl-8">I am currently developing a furniture e-commerce website using Node.js and EJS. The project involves creating the website's layout and functionality</p>
+              </div>
+              <div className="md:w-1/2 order-1 relative md:order-2">
+                <h4 className="text-2xl bLine2  tracking-widest px-2">Tswitch (running)</h4>
+                <p className="pTextColor text-xl px-2">Furniture E-commerce</p>
+                <div className="projImg mt-4 md:ml-28 ml-24" >
+                  {/* Use project-specific image if available */}
+                  <img src="/images/twitch.png" alt="" />
+                </div>
+              </div>
             </div>
             {/* projecct featched higlight */}
             {data?.Projects.filter(project => project.highlightProject === true).map((project, index) => (
@@ -187,7 +218,7 @@ const page = () => {
               <h3 className='text-3xl font-bold'>Softwaer I use</h3>
             </div>
             <div className='px-2 mt-12 md:w-1/2 w-full'>
-              <div className='flex flex-wrap'>
+              <div className='flex flex-wrap justify-center'>
                 <div className="img">
                   <img src="/images/flowbite_html-solid.svg" alt="html" />
                 </div>
@@ -242,16 +273,40 @@ const page = () => {
             ))
             }
             <div className='relative'>
-              <div className="type absolute left-0 top-4 px-8 py-4 tBgColor rounded-e-md">
-                <p className='text-lg'>cover art</p>
+              <div className="type absolute left-0 top-4 px-8 py-2 tBgColor rounded-e-md">
+                <p className='text-lg'>Kurv</p>
               </div>
-              <img src="/images/pr1.png" alt="" className='w-full' />
+              <img src="/images/kurv.png" alt="" className='w-full' />
+            </div>
+            <div className='relative'>
+              <div className="type absolute left-0 top-4 px-8 py-2 tBgColor rounded-e-md">
+                <p className='text-lg'>Loading Walla</p>
+              </div>
+              <img src="/images/lw.png" alt="" className='w-full' />
+            </div>
+            <div className='relative'>
+              <div className="type absolute left-0 top-4 px-8 py-2 tBgColor rounded-e-md">
+                <p className='text-lg'>Loading Walla CRM</p>
+              </div>
+              <img src="/images/lwcrm.png" alt="" className='w-full' />
+            </div>
+            <div className='relative'>
+              <div className="type absolute left-0 top-4 px-8 py-2 tBgColor rounded-e-md">
+                <p className='text-lg'>Tswitch</p>
+              </div>
+              <img src="/images/twitch.png" alt="" className='w-full' />
+            </div>
+            <div className='relative'>
+              <div className="type absolute left-0 top-4 px-8 py-2 tBgColor rounded-e-md">
+                <p className='text-lg'>Tour and Trevels</p>
+              </div>
+              <img src="/images/tourandtrevel.png" alt="" className='w-full' />
             </div>
           </div>
         </div>
 
       </section>
-      <section id="work" className='pBgColor text-white'>
+      <section id="work" className='pBgColor text-white hidden'>
         <div className="container mx-auto">
           <p className='pTextColor text-center py-4'>Work History</p>
           <h3 className='text-3xl font-bold text-center'>Working with Passion</h3>
@@ -277,9 +332,9 @@ const page = () => {
           </div>
         </div>
       </section>
-      <section id='footer' className='pBgColor text-white p-4'>
+      <section id='footer' className='pBgColor text-white p-4 pb-16'>
         <div className='container mx-auto'>
-          <div className='grid md:grid-cols-5 grid-cols-1 gap-4'>
+          <div className='grid md:grid-cols-4 grid-cols-1 gap-4'>
             <div>
               <h3 className='underline underline-offset-4 font-bold text-2xl'>Contact me:</h3>
             </div>
@@ -291,12 +346,17 @@ const page = () => {
             </div>
             <div className='flex md:justify-center justify-start items-center'>
               <div><p>Social media:- </p></div>
-              <div className='mx-2'><p className='pTextColor'>icons</p></div>
+              <div className='mx-2'>
+                <a href="https://www.linkedin.com/in/himanshu-gautam-5185921b3/">
+                  {/* <p className='pTextColor'>icons</p> */}
+                  <img src="/images/linkedin.svg" alt="" />
+                </  a>
+              </div>
             </div>
-            <div className='flex md:justify-center justify-start items-center'>
+            {/* <div className='flex md:justify-center justify-start items-center'>
               <div><p>resume:-</p></div>
               <div><button className='px-3 py-2 Dbtn mx-2'>Download</button></div>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
